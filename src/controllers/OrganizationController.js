25ee9abb24c9f6bdc57518b9a0e51dbd03639c82
Bob Hageman <b.hageman@mybo.nu>
2020-10-11 13:12:14 +0200

    added dynamic pdf with qr code generation, updated login + session (fixed a problem where sometimes you needed to login twice in order to set the session). cleanup dependencies

--- src/controllers/OrganizationController.js
+++ src/controllers/OrganizationController.js
@@ -223,7 +223,7 @@ const OrganizationController = {
 	
 	login : function (req, res){
 		
-		// check if user is already logged in, then skip login page
+		// check if user is already logged in. If so, skip login page
 		if (req.session.user) {
 			res.message('U bent al ingelogd');
 			return res.redirect('account');
@@ -237,34 +237,228 @@ const OrganizationController = {
 
 	processLogin : async (req, res, next) => {
 
-		let org = await Organization.findOne({ email : req.body.email});
-		
-		if (!org || await !org.comparePassword(req.body.password))
-		{
+		let returnInvalid = () =>	{
 			res.message('E-mail adres of wachtwoord onjuist');
 			
-			return res.render('organization/login', {
+			res.render('organization/login', {
 				layout : 'main',
 				csrfToken : req.csrfToken()
 			});
 		}
+
+		let org = await Organization.findOne({ email : req.body.email});
 		
-		req.session.user = org;
-		return res.redirect('/account');
+		if (!org)
+			return returnInvalid();
 			
+		let compareResult = await org.comparePassword(req.body.password);
+		
+		if (compareResult != true)
+			return returnInvalid();
+			
+		// set session
+		req.session.user = { 
+			_id : org.id,
+			name : org.name
+		};
+		
+		return res.redirect('/account');
 	},
 	
 	processCreateQR : async function (req, res){
-	
+
+		/* when we need the qr as image in the future:
 		const qr = require('qr-image');
-		var qr_svg = qr.image('http://localhost:3000/check-in/' + req.session.user._id, { 
-			type: 'svg',
-			parse_url : true,
-			size:10
-			});
+		var svg_string = qr.imageSync('http://localhost:3000/check-in/' + req.session.user._id, { type: 'png', parse_url : true,
+			size:10 });		
+		*/
+		
+		var PdfPrinter = require('pdfmake');
+		let fs = require('fs');
+		// Define font files
+		var fonts = {
+			Walsheim: {
+				normal : __dirname + '/../static/fonts/GT-Walsheim-Regular.ttf',
+				bold :  __dirname + '/../static/fonts/GT-Walsheim-Bold.ttf'
+			}
+		};		
+		
+		var printer = new PdfPrinter(fonts);
+		
+		var docDefinition = {
+		  // a string or { width: number, height: number }
+		  pageSize: 'A4',
+
+		  // by default we use portrait, you can change it to landscape if you wish
+		  pageOrientation: 'portrait',
+
+		  // [left, top, right, bottom] or [horizontal, vertical] or just a number for equal margins
+		  pageMargins: [ 40, 20, 40, 60 ],			
 			
-		res.type('svg');
-		qr_svg.pipe(res);
+			background : [
+				{	image : __dirname + '/../static/img/pdf-background.png', width: 599 }
+			],
+
+		  info: {
+			title: 'Check-in met QR code',
+			author: 'Present - by Schluss',
+			subject: '',
+			keywords: '',
+		  },			
+					
+		  content: [
+		  
+			// logo image
+			{
+				svg:  fs.readFileSync(__dirname + '/../static/img/logo.svg', 'utf8'),
+				alignment : 'center',
+				width : 75
+			},	
+
+			// title
+			{ text: '\nLaat weten dat je er bent', bold: true, fontSize: 30, alignment : 'center' },
+
+			// text
+			{ text: '\nDeel je gegevens voor een mogelijk bron\n en contactonderzoek door de GGD\n\n\n\n', fontSize: 15, color : '#666666', alignment : 'center', height: 200 },
+			
+			// marker 
+			{ svg:  fs.readFileSync(__dirname + '/../static/img/marker.svg', 'utf8'), width : 15, alignment : 'center' },
+			
+			// organization name
+			{
+				width: '50%',
+				text : req.session.user.name,
+				bold: true, 
+				fontSize: 20, 
+				alignment : 'center'
+			},
+						
+			// spacer
+			{ text : '\n\n'},			
+
+			// check items
+			{			  
+				columns : [
+				
+					{ text : '', width: '22%' },
+					
+					{
+						width: '8%',
+						svg:  fs.readFileSync(__dirname + '/../static/img/check.svg', 'utf8'),
+						width : 18,
+					},
+
+					{ text : '', width: '1%' },					
+					
+					{
+						width : '20%',
+						text: 'Anoniem', fontSize: 15
+					},
+					
+					{
+						width: '8%',
+						svg:  fs.readFileSync(__dirname + '/../static/img/check.svg', 'utf8'),
+						width : 18
+					},	
+
+					{ text : '', width: '1%' },						
+					
+					{
+						width : '*',
+						text: 'Niet verplicht, wel lief', fontSize: 15
+					}			
+				
+				]
+				
+			},
+			{			  
+				columns : [
+				
+					{ text : '', width: '22%' },
+					
+					{
+						svg:  fs.readFileSync(__dirname + '/../static/img/check.svg', 'utf8'),
+						width : 18
+					},	
+
+					{ text : '', width: '1%' },						
+					
+					{
+						width : '20%',
+						text: 'Veilig', fontSize: 15
+					},
+					
+					{
+						svg:  fs.readFileSync(__dirname + '/../static/img/check.svg', 'utf8'),
+						width : 18
+					},	
+
+					{ text : '', width: '1%' },						
+					
+					{
+						width : '*',
+						text: 'Na 14 dagen vernietigd', fontSize: 15
+					}				
+				
+				]
+				
+			},	
+
+			{ text: '\n', fontSize: 20 },
+
+			// qr code
+			{ qr: 'http://localhost:3000/check-in/' + req.session.user._id, eccLevel : 'M', fit : 200, alignment: 'center' },
+		  
+			// spacer
+			{ text : '\n\n\n\n\n\n\n' },
+		  
+		  
+			// powered and secured by
+			{
+				columns : [
+			  
+					{ text : '', width: '35%' },
+					
+					{
+						svg:  fs.readFileSync(__dirname + '/../static/img/lock.svg', 'utf8'),
+						width : 10
+					},
+					
+					{ text : '', width: '1%' },					
+					
+					{
+						text : 'secured and powered by',
+						color : '#7e879c',
+						width: '18%',
+						fontSize: 8
+					},	
+
+					{
+						svg:  fs.readFileSync(__dirname + '/../static/img/schluss-logo.svg', 'utf8'),
+						width : 55
+					}					
+			  
+				] 
+		  }
+		  
+		  ],
+
+  			defaultStyle: {
+				font: 'Walsheim'
+			}
+		};
+
+		var options = {
+		  // ...
+		};
+		
+		var pdfDoc = printer.createPdfKitDocument(docDefinition, options);
+		
+		// send pdf to browser as download
+		res.setHeader('Content-Type', 'application/pdf');
+		res.setHeader('Content-Disposition', 'attachment; filename=QR-code.pdf');
+		pdfDoc.pipe(res);		
+		pdfDoc.end();
 	},
 	
 	logout : function(req, res){
